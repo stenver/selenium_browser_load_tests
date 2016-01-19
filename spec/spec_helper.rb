614c1a38d93f8db6dccf42fd140e402c22b28bb3
Stenver Jerkku <stenver1010@gmail.com>
2016-01-19 19:41:12 +0200

    Make startfile run in rspec mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,30 @@ RSpec.configure do |config|
     firefox
   end
 
+  def open_browser
+    puts "warmuping #{ENV[:TEST_ENV_NUMBER.to_s].inspect}"
+    sleep 5
+    puts "Started a loop in #{ENV[:TEST_ENV_NUMBER.to_s].inspect}"
+
+    while true do
+      fox = open_browser(:register_firefox_driver)
+      puts "Browser opened with #{ENV[:TEST_ENV_NUMBER.to_s].inspect}"
+      begin
+        fox.visit("http://www.delfi.ee")
+        puts "Visited delfi with #{ENV[:TEST_ENV_NUMBER.to_s].inspect}"
+        10.times do
+          sleep 1
+          fox.execute_script "window.scrollBy(0,1000)"
+          sleep 1
+          puts "#{ENV[:TEST_ENV_NUMBER.to_s].inspect} scrolling..."
+        end
+      rescue Exception => e
+        puts e.message
+        puts e.backtrace
+      end
+    end
+  end
+
   config.raise_errors_for_deprecations!
   config.mock_with :rspec
   configure_capybara
---
+++ spec/tests/2_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+require 'securerandom'
+
+describe { it { open_browser  } }
